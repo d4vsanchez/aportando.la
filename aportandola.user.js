916c0beb7ec2abe2720602f368387d7085d1526b
David Sanchez <d4vsanchez@gmail.com>
2021-11-21 18:27:28 -0500

    Add image button

--- aportandola.user.js
+++ aportandola.user.js
@@ -5,10 +5,44 @@
 // @description  Mejora el editor de aportes de Platzi en los cursos
 // @author       David Sánchez
 // @match        https://platzi.com/clases/*
+// @require      https://kit.fontawesome.com/ee48b15041.js
 // ==/UserScript==
 
 (function() {
   'use strict';
+
+  const lib = {
+    style: function (selector, rules) {
+      const styleTag = document.createElement('style');
+      styleTag.textContent = `${selector} { ${rules.join(';')} }`;
+      document.getElementsByTagName('head')[0].appendChild(styleTag);
+    },
+    getCookies: function () {
+      const cookies = decodeURIComponent(document.cookie);
+
+      return cookies.split('; ').reduce((result, cookie) => {
+        const [name, value] = cookie.split('=');
+        return { ...result, [name]: value };
+      }, {});
+    },
+    getCsrfToken: function () {
+      return this.getCookies().csrftoken;
+    },
+    getSelectedText: function (editor) {
+      return editor.value.substring(editor.selectionStart, editor.selectionEnd);
+    },
+    appendTextEditor: function (text) {
+      const textEditor = ui.editor();
+
+      textEditor.setRangeText(text);
+      textEditor.focus();
+    },
+    markdown: {
+      image: function (imageName, imagePath) {
+        return `![${imageName}](${imagePath})`;
+      }
+    }
+  }
 
   const ui = {
     buildButton: function () {
@@ -23,7 +57,10 @@
       return newDivider;
     },
     menuBar: function () {
-      return document.querySelector('.Pulseeditor-toolbar-left');
+      return document.querySelector('.PulseEditor-toolbar-left');
+    },
+    editor: function () {
+      return document.querySelector('textarea.PulseEditor-field');
     },
     editorInput: function () {
       return document.querySelector('.EditorWrapper-input');
@@ -31,10 +68,83 @@
     editorInputClose: function () {
       return document.querySelector('.EditorWrapper-form-close');
     },
+    getOriginalButton: function (buttonName) {
+      return document.querySelector(`.PulseEditor-button[name=${buttonName}]`);
+    },
     appendMenuButton: function (menuButton) {
       this.menuBar().appendChild(menuButton);
     },
+    removeMenuButton: function (element) {
+      element.remove();
+    }
   };
+
+  const api = {
+    inProgress: 0,
+    call: function (method, url, body) {
+      return fetch(url, {
+        method: method,
+        body: body,
+        credentials: 'same-origin',
+        headers: {
+          'x-csrftoken': lib.getCsrfToken(),
+        }
+      }).then(
+        response => response.json()
+      );
+    },
+    uploadImage: function (imageFile) {
+      const formData = new FormData();
+      formData.append('file', imageFile);
+
+      return this.call('post', '/upload_images/', formData);
+    }
+  }
+
+  const styles = {
+    '.aportandola-upload_image': [
+      'opacity: 0',
+      'position: absolute',
+      'top: 0',
+      'bottom: 0',
+      'right: 0',
+      'left: 0',
+    ],
+    '.PulseEditor-button': [
+      'position: relative',
+      'overflow: hidden',
+    ]
+  }
+
+  for (const selector in styles) {
+    lib.style(selector, styles[selector]);
+  }
+
+  // #region Menu bar icons
+
+  const renderImageButton = function () {
+    const imageButton = ui.buildButton();
+    imageButton.innerHTML = `
+      <i class="far fa-file-image"></i>
+      <input type="file" class="aportandola-upload_image" />
+    `;
+
+    const fileInput = imageButton.querySelector('.aportandola-upload_image');
+    fileInput.addEventListener('change', function (event) {
+      const selectedFile = event.target.files[0];
+      if (!selectedFile) return;
+
+      api.uploadImage(selectedFile).then((response) => {
+        const markdownText = lib.markdown.image(selectedFile.name, response.path);
+        lib.appendTextEditor(markdownText);
+      });
+    });
+
+    ui.appendMenuButton(imageButton);
+    ui.removeMenuButton(ui.getOriginalButton('image'));
+  }
+
+  // #endregion
 
   const onEditorShow = function (cb) {
     ui.editorInput().addEventListener('click', () => {
@@ -50,6 +160,7 @@
 
   const buildMenuBar = function () {
     onEditorShow(() => {
+      renderImageButton();
       onEditorHide(buildMenuBar);
     });
   }
